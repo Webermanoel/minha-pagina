e728a7f160a341cb2a4c05e4b01b8163c39e98d5
Webermanoel <webermanoel22@gmail.com>
2024-12-07 14:51:23 -0400

    minha-pagina

--- arquivos.js/programa.js
+++ arquivos.js/programa.js
@@ -1,18 +1,18 @@
 let itensDaLista = [];
-let limiteItens = 10;
+const limiteDeItens = 10;
 
-
-function exibirTextoNaTela(tag, texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
+function exibirTextoNaTela(tag, texto) {
+  const elemento = document.createElement(tag);
+  elemento.innerText = texto;
 }
 
 exibirTextoNaTela('h1', 'Bem-vindo ao programa de cadastro de itens');
 
+
 function salvarItens(){
   let itensCadastrados = document.querySelector('input').value;
 
-  if(itensCadastrados <= 10){
+  if(itensCadastrados == itensDaLista.length){
     exibirTextoNaTela('H1', 'Deseja salvar os itens?')
   }
 
@@ -20,13 +20,17 @@ function salvarItens(){
 
   function limparCampo() {
     chute = document.querySelector('input');
-    chute.itensCadastrados = 10;
+    chute.itensCadastrados = itensEscolhidos;
 }
 
 function reiniciarLista(){
+
 
 }
 
 function mostrarLista(){
 
 }
+
+salvarItens();
+
